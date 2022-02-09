9e6736144304cab589a2a3a953b430b100081c81
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-02-09 23:51:17 +0000

    spin animation on click

--- script.js
+++ script.js
@@ -2,17 +2,127 @@
 // Needs to include scale e.g. transform: scale(2.3) translateY(60px);
 // 
 //      Slot 1                  Slot 2                  Slot 3
-//      1 Github    120px       1 HTML5     120px       1 Android   120px
-//      2 HTML5     60px        2 Github    60px        2 Ruby      60px
-//      3 React     0px         3 React     0px         3 React     0px
-//      4 Ruby      -60px       4 Android   -60px       4 HTML5    -60px
-//      5 Android   -120px      5 Ruby      -120px      5 Github    -120px
+//      1 Github    120px       3 HTML5     120px       2 Android   120px
+//      3 HTML5     60px        1 Github    60px        4 Ruby      60px
+//      5 React     0px         5 React     0px         5 React     0px
+//      4 Ruby      -60px       2 Android   -60px       3 HTML5    -60px
+//      2 Android   -120px      4 Ruby      -120px      1 Github    -120px
 //
 //  Matches: 
 // 
-//  1, 2 & 5 = Github   100 points
-//  5, 4 & 1 = Android  200 points
-//  2, 1 & 4 = HTML5    300 points
-//  4, 5 & 2 = Ruby     400 points
-//  3, 3 & 3 = React    500 points
+//  1,1,1 = Github   100 points
+//  2,2,2 = Android  200 points
+//  3,3,3 = HTML5    300 points
+//  4,4,4 = Ruby     400 points
+//  5,5,5 = React    500 points
+
+// Generate random number 
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// Get element by ID
+
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+// Move slots up or down
+
+function upOne(el) {
+    el.classList.add('upOne');
+}
+
+function upTwo(el) {
+    el.classList.add('upTwo')
+}
+
+function downOne(el) {
+    el.classList.add('downOne')
+}
+
+function downTwo(el) {
+    el.classList.add('downTwo')
+}
+
+// Reset slots
+
+function reset(el) {
+    el.classList.remove('upOne');
+    el.classList.remove('upTwo');
+    el.classList.remove('downOne');
+    el.classList.remove('downTwo');
+    el.classList.remove('marqueeOne');
+    el.classList.remove('marqueeTwo');
+    el.classList.remove('marqueeThree');
+    void el.offsetWidth; // https://css-tricks.com/restart-css-animation/
+}
+
+// Animation 
+
+// Click to spin
+
+
+
+function spin() {
+
+const slotOne = getElement('slotOne');
+const slotTwo = getElement('slotTwo');
+const slotThree = getElement('slotThree');
+
+reset(slotOne);
+reset(slotTwo);
+reset(slotThree);
+
+slotOne.classList.add('marqueeOne');
+slotTwo.classList.add('marqueeTwo');
+slotThree.classList.add('marqueeThree');
+
+const icon1 = getRandomNumber();
+const icon2 = getRandomNumber();
+const icon3 = getRandomNumber();
+
+console.log(icon1, icon2, icon3);
+
+if (icon1 === 1) {
+    upTwo(slotOne)
+} else if (icon1 === 2) {
+    downTwo(slotOne)
+} else if (icon1 === 3) {
+    upOne(slotOne)
+} else if (icon1 === 4) {
+    downOne(slotOne)
+}
+
+if (icon2 === 1) {
+    upOne(slotTwo)
+} else if (icon2 === 2) {
+    downOne(slotTwo)
+} else if (icon2 === 3) {
+    upTwo(slotTwo)
+} else if (icon2 === 4) {
+    downTwo(slotTwo)
+}
+
+if (icon3 === 1) {
+    downTwo(slotThree)
+} else if (icon3 === 2) {
+    upTwo(slotThree)
+} else if (icon3 === 3) {
+    downOne(slotThree)
+} else if (icon3 === 4) {
+    upOne(slotThree)
+}
+
+}
+
+const spinBtn = getElement('spinBtn')
+
+spinBtn.addEventListener("click", spin);
+
+
+
+
+
 
